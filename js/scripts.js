6a035d670cce890cf157cc4b84191882f144ead1
Matt Luker <jamesmattluker@gmail.com>
2021-11-05 15:54:53 -0600

    add a no topping radio option in each topping card

--- js/scripts.js
+++ js/scripts.js
@@ -44,19 +44,34 @@ Pizza.prototype.calculateTotalPrice = function() {
 }
 
 // UI
+$("input[type=radio]").each(function() {
+  var secondClick = true;
+  $(this).change(function() {
+      secondClick = false;
+  });
+  $(this).click(function() {
+      if (secondClick) {
+          $(this).prop("checked", false);
+      }
+      secondClick = true;
+  });
+});
 
 $(document).ready(function() {
   $("form#pizza-order-form").submit(function(event) {
     event.preventDefault();
     const sizeChoice = $("input:radio[name=size]:checked").val();
-    const topping1Choice = $("input:radio[name=topping1]:checked").val()
-    const topping2Choice = $("input:radio[name=topping2]:checked").val()
-    const topping3Choice = $("input:radio[name=topping3]:checked").val()
-    const topping4Choice = $("input:radio[name=topping4]:checked").val()
-    const topping5Choice = $("input:radio[name=topping5]:checked").val()
+    const topping1Choice = $("input:radio[name=topping1]:checked").val();
+    const topping2Choice = $("input:radio[name=topping2]:checked").val();
+    const topping3Choice = $("input:radio[name=topping3]:checked").val();
+    const topping4Choice = $("input:radio[name=topping4]:checked").val();
+    const topping5Choice = $("input:radio[name=topping5]:checked").val();
 
-    const yourPizza = new Pizza([topping1Choice, topping2Choice, topping3Choice, topping4Choice, topping5Choice], sizeChoice)
-    console.log(yourPizza)
+    const yourPizza = new Pizza([topping1Choice, topping2Choice, topping3Choice, topping4Choice, topping5Choice], sizeChoice);
+    yourPizza.toppings = yourPizza.toppings.filter(function( element ) {
+      return element !== 'no topping';
+    });    
+    console.log(yourPizza);
     $("ul.output").text("Thank you for your order! Your pizza will cost $" + yourPizza.calculateTotalPrice());
   });
 });
